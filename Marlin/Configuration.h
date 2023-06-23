4a2249886b145235d1ea4aed118bf0b88d0f57ff
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2023-06-23 10:39:39 -0500

    configured for Ender 3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1777,7 +1777,7 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 200
+#define X_BED_SIZE 235
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
@@ -1786,7 +1786,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 210
+#define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
